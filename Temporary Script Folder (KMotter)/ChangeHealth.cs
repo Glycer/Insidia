1a69cb2fdfd3d76a68883d896033572fed3a5eed
Unknown <hopskipbye@gmail.com>
2017-09-03 20:53:56 -0600

    Script Upload

---
+++ Temporary Script Folder (KMotter)/ChangeHealth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ChangeHealth : MonoBehaviour {
+
+    /* As a placeholder, I'm referring to whatever script will carry health data
+     * as 'StatScript'
+     */
+
+    public static Action</* StatScript, */ float> Damage;
+    public static Action</* StatScript, */ float> Restore;
+
+    void Start()
+    {
+        Damage += ApplyDamage;
+        Restore += ApplyRestore;
+    }
+
+    void ApplyDamage(/* StatScript, */ float _damage)
+    {
+        //StatScript.health -= _damage;
+    }
+
+    void ApplyRestore(/* StatScript, */ float _restored)
+    {
+        //StatScript.health += _restored;
+    }
+}
